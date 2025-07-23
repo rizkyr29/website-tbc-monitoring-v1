d4003fe0500ea7251da09f1faa3742622410f4be
Mochamad Rizky Ramadhan <rizkyr29@gmail.com>
2025-07-24 00:28:31 +0700

    pindah index.html

--- frontend/script.js
+++ frontend/script.js
@@ -43,5 +43,13 @@ function goBack() {
 }
 
 function addPasien() {
-    // Implementasi untuk menambah pasien
+router.post('/', (req, res) => {
+  const { nama, nik, alamat, tanggal_lahir, keterangan, detail_alamat, status_tbc, nomor_telepon } = req.body;
+  const query = 'INSERT INTO pasien (nama, nik, alamat, tanggal_lahir, keterangan, detail_alamat, status_tbc, nomor_telepon) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+  
+  db.query(query, [nama, nik, alamat, tanggal_lahir, keterangan, detail_alamat, status_tbc, nomor_telepon], (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    res.status(201).json({ message: 'Pasien added successfully' });
+  });
+});
 }
